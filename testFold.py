f78f6b8651c94db9e211cf8158bba193b982a4bd
Sertan Senturk <contact@sertansenturk.com>
2015-09-22 01:32:05 +0200

    Converted testFold to work on local

--- testFold.py
+++ testFold.py
@@ -13,7 +13,7 @@ from ModeTonicEstimation import Evaluator as ev
 
 
 # I/O
-input_num = int(sys.argv[1])-1
+#input_num = int(sys.argv[1])-1
 
 base_dir = '../../experiments/raag-recognition/'
 data_dir = os.path.join(base_dir,'data')
@@ -28,40 +28,40 @@ che = Chordia.Chordia(step_size=10, smooth_factor=15, chunk_size=120,
 # indexing
 n_exp = 20
 n_folds = 12
+for input_num in xrange(0, 240):
+  mode_idx = np.unravel_index(input_num, [n_exp, n_folds])
 
-mode_idx = np.unravel_index(input_num, [n_exp, n_folds])
-
-trainFolder = os.path.join(experiments_dir, 'exp' + str(mode_idx[0]), 
-                           'fold' + str(mode_idx[1]), 'train')
-resultFile = os.path.join(experiments_dir, 'exp' + str(mode_idx[0]), 
-                          'fold' + str(mode_idx[1]), 'results.json')
-
-
-# create load the raag from the experiment & fold corresponding to the input index
-foldFile = os.path.join(experiments_dir, 'exp' + str(mode_idx[0]), 'folds.json')
+  trainFolder = os.path.join(experiments_dir, 'exp' + str(mode_idx[0]), 
+                             'fold' + str(mode_idx[1]), 'train')
+  resultFile = os.path.join(experiments_dir, 'exp' + str(mode_idx[0]), 
+                            'fold' + str(mode_idx[1]), 'results.json')
 
 
-with open(foldFile) as f:
-    folds = json.load(f)
-        
-makamTestFiles = folds['fold'+str(mode_idx[1])]['test']
+  # create load the raag from the experiment & fold corresponding to the input index
+  foldFile = os.path.join(experiments_dir, 'exp' + str(mode_idx[0]), 'folds.json')
 
 
-# Raag Recognition
-results = []
-for rec in makamTestFiles:
-    rec['pitch'] = np.loadtxt(rec['file'])[:,1]
-    res = che.estimate(rec['pitch'], mode_names=modes, mode_dir=trainFolder, 
-                       est_mode=True, distance_method='bhat',
-                       metric='pcd', tonic_freq=rec['tonic'], 
-                       equalSamplePerMode=True)[0]
+  with open(foldFile) as f:
+      folds = json.load(f)
+          
+  makamTestFiles = folds['fold'+str(mode_idx[1])]['test']
 
-    # evaluate
-    results.append(evaluator.mode_evaluate(rec['file'], res, rec['mode']))
 
-print('exp' + str(mode_idx[0]) + '_fold' + str(mode_idx[1]) + ":" 
-      " " + str(100*np.mean([r['mode_eval'] for r in results])) + '%')
+  # Raag Recognition
+  results = []
+  for rec in makamTestFiles:
+      rec['pitch'] = np.loadtxt(rec['file'])[:,1]
+      res = che.estimate(rec['pitch'], mode_names=modes, mode_dir=trainFolder, 
+                         est_mode=True, distance_method='bhat',
+                         metric='pcd', tonic_freq=rec['tonic'], 
+                         equalSamplePerMode=True)[0]
 
-with open(resultFile, 'w') as r:
-    json.dump(results, r, indent=4)
+      # evaluate
+      results.append(evaluator.mode_evaluate(rec['file'], res, rec['mode']))
+
+  print('exp' + str(mode_idx[0]) + '_fold' + str(mode_idx[1]) + ":" 
+        " " + str(100*np.mean([r['mode_eval'] for r in results])) + '%')
+
+  with open(resultFile, 'w') as r:
+      json.dump(results, r, indent=4)
 
