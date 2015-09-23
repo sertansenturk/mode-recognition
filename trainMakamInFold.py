ed08efbc0d029505d780cd8c0b2e092f4a10d294
Sertan Senturk <contact@sertansenturk.com>
2015-09-23 15:39:55 +0200

    Reverted back to external indexing for the cluster

--- trainMakamInFold.py
+++ trainMakamInFold.py
@@ -21,24 +21,25 @@ che = Chordia.Chordia(step_size=10, smooth_factor=15, chunk_size=0,
 # indexing
 n_exp = 20
 n_folds = 12
-n_modes = len(modes)  # 10 raagas
+n_modes = len(modes)
 
+input_num = int(sys.argv[1])-1
 
-for ex_idx in xrange(0,n_exp):
-	for fo_idx in xrange(0, n_folds):
-		for cur_mode in modes:
-			# create load the raag from the experiment & fold corresponding to the input index
-			foldFile = os.path.join(experiments_dir, 'exp' + str(ex_idx), 'folds.json')
-			with open(foldFile) as f:
-			    folds = json.load(f)
-			        
-			makamTrain_dict = folds['fold'+str(fo_idx)]['train']
+for input_num in range(0, n_exp*n_folds*n_modes):
 
-			[file_list, tonic_list] = zip(*[(rec['file'], rec['tonic']) for rec in makamTrain_dict
-			                                if rec['mode'] == cur_mode])
+	mode_idx = np.unravel_index(input_num, [n_exp, n_folds, n_modes])
+	# create load the raag from the experiment & fold corresponding to the input index
+	foldFile = os.path.join(experiments_dir, 'exp' + str(ex_idx), 'folds.json')
+	with open(foldFile) as f:
+	    folds = json.load(f)
+	        
+	makamTrain_dict = folds['fold'+str(fo_idx)]['train']
 
-			train_savefolder = os.path.join(experiments_dir, 'exp' + str(ex_idx), 
-			                                'fold' + str(fo_idx), 'train')
-			model = che.train(cur_mode, file_list, tonic_list, metric='pcd', 
-			                             save_dir = train_savefolder)
-			print "Finished training: " + os.path.join(train_savefolder, cur_mode)
+	[file_list, tonic_list] = zip(*[(rec['file'], rec['tonic']) for rec in makamTrain_dict
+	                                if rec['mode'] == cur_mode])
+
+	train_savefolder = os.path.join(experiments_dir, 'exp' + str(ex_idx), 
+	                                'fold' + str(fo_idx), 'train')
+	model = che.train(cur_mode, file_list, tonic_list, metric='pcd', 
+	                             save_dir = train_savefolder)
+	print "Finished training: " + os.path.join(train_savefolder, cur_mode)
